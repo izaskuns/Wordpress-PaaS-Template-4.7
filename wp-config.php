48b27a7ef11b46714e96c3863ead6498b5188c8d
Izaskun Santisteban Gómez <izaskun.santisteban.gomez@es.ibm.com>
2017-02-24 14:07:33 -0600

    Deshardcodeados parámetros y cambiada forma de obtener Credential de la bbdd de VCAP_SERVICES.

--- wp-config.php
+++ wp-config.php
@@ -2,9 +2,9 @@
 $services = getenv("VCAP_SERVICES");
 $services_json = json_decode($services,true);
 
-$mysql_config = $services_json[“cleardb”][0][“credentials”];
+//$mysql_config = $services_json[“cleardb”][0][“credentials”];
 /*$mysql_config = $services_json[“cleardb”];*/
-/*$mysql_config = $services_json[“cleardb”][“credentials”];*/
+$mysql_config = $services_json[“cleardb”][“credentials”];
 error_log(“PROBANDO”);
 error_log($mysql_config);
 
@@ -21,19 +21,19 @@ error_log($mysql_config["host"], 0);
 error_log("Port");
 error_log($mysql_config["port"], 0);
 
-//define('DB_NAME', $mysql_config["name"]);
-define('DB_NAME', 'ad_2d0cbdc980e554c');
+define('DB_NAME', $mysql_config["name"]);
+//define('DB_NAME', 'ad_2d0cbdc980e554c');
 
 /*define('DB_USER', $mysql_config["user"]);*/
-//define('DB_USER', $mysql_config["username"]);
-define('DB_USER', 'b0d208ec37c35a');
+define('DB_USER', $mysql_config["username"]);
+//define('DB_USER', 'b0d208ec37c35a');
 
-//define('DB_PASSWORD', $mysql_config["password"]);
-define('DB_PASSWORD', '97e1ea5f');
+define('DB_PASSWORD', $mysql_config["password"]);
+//define('DB_PASSWORD', '97e1ea5f');
 
 /*define('DB_HOST', $mysql_config["host"].":".$mysql_config["port"]);*/
-//define('DB_HOST', $mysql_config["hostname"].":".$mysql_config["port"]);
-define('DB_HOST', "us-cdbr-iron-east-04.cleardb.net".":"."3306");
+define('DB_HOST', $mysql_config["hostname"].":".$mysql_config["port"]);
+//define('DB_HOST', "us-cdbr-iron-east-04.cleardb.net".":"."3306");
 /*define('DB_PORT', $mysql_config["port"]);*/
 
 define('DB_CHARSET', 'utf8');
